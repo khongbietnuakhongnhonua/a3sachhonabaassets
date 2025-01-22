20d6ecfb93e42f0ce06721415b024a6af28b6f5b
Nguyễn Minh Khôi <nguyenminhkhoi2009.nmk@gmail.com>
2025-01-22 13:10:41 +0700

    Buff bẩn

---
+++ secret/code/K/CodeTemplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define task ""
+using namespace std;
+
+
+
+void ReadData(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    freopen(task".INP", "r", stdin);
+    freopen(task".OUT", "w", stdout);
+    //Input
+}
+
+void Solve(){
+    //Solve
+
+}
+
+void Print(){
+    //Print
+
+}
+
+int main(){
+    ReadData();
+    Solve();
+    Print();
+    return 0;
+}
